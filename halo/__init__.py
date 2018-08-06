6d72b9c92e1bd9d91633b9e57b3d13d3edb95561
Tyler Souza <tsouza.sf@gmail.com>
2018-08-06 16:49:39 -0700

    renamed routes file to views.py.

--- halo/__init__.py
+++ halo/__init__.py
@@ -8,4 +8,4 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-from src import routes, models
+from halo import views, models
--- halo/views.py
+++ halo/views.py
@@ -2,6 +2,5 @@ from flask import render_template
 from halo import app
 
 @app.route('/')
-@app.route('/index')
 def index():
     return render_template("index.html")
